bfcfca922f7df145a55eb2d4cf32fb03581cdd6e
lorenzohill <lorenzokhill985@gmail.com>
2022-06-25 23:35:01 -0500

    Initial Commit

---
+++ rps.js
@@ -0,0 +1,18 @@
+console.log("Hello World")
+
+
+
+// Gets CPU input
+function cpuSelect(){
+
+}
+
+// Gets Player Input
+function playerInput() {
+    
+}
+
+// Main game loop
+function mainLoop() {
+    
+}
