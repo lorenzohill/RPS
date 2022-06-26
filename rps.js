d4dc0b3905ab67751c094d632c9d694c39554065
lorenzohill <lorenzokhill985@gmail.com>
2022-06-26 01:04:28 -0500

    Added JS code for RPS game

--- rps.js
+++ rps.js
@@ -1,18 +1,59 @@
-console.log("Hello World")
-
-
-
+let wins = 0
 // Gets CPU input
 function cpuSelect(){
-
+let select = ["rock", "paper","scissors"]
+select = select[Math.floor(Math.random()*select.length)];
+return select
 }
 
 // Gets Player Input
-function playerInput() {
-    
+function playRound(player, CPU) {
+player = player.toLowerCase()
+console.log("Player Chose " + player)
+console.log("CPU selected " + CPU)
+if (player === CPU){
+  console.log("You Tie.")
+}
+else if(player === "rock"){
+  if (CPU === "scissors"){
+    console.log("Rock Beats Scissors. You win")
+    wins = wins + 1
+    return wins
+  }
+  else {
+    console.log("Paper covers Rock. You lose.")
+  }
+
+}
+else if (player === "paper"){
+  if (CPU === "rock"){
+    console.log("Paper cover rock. You win")
+    wins = wins + 1
+    return wins
+  }
+  else {
+    console.log("Scissors cut Paper. You lose")
+  }
+}
+else if(player === "scissors"){
+  if (CPU === "paper"){
+    console.log("Scissors cut paper. You win")
+    wins = wins + 1
+    return wins
+  }
+  else{
+    console.log("Rock Beats Scissors. You Lose")
+  }
+}
 }
 
 // Main game loop
 function mainLoop() {
-    
+ // let player = prompt("Enter Rock, Paper or Scissors")  
+ // Below code is for Debug
+ player = ["rock", "paper","scissors"]
+ player = player[Math.floor(Math.random()*player.length)];
+ CPU = cpuSelect()
+playRound(player, CPU)
+console.log("You have " + wins + " wins")
 }
